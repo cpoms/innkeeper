influitive/apartment

Run migrations in parallel

lib/tasks/apartment.rake
@@ -1,10 +1,11 @@
 require 'apartment/migrator'
+require 'parallel'
 
 apartment_namespace = namespace :apartment do
 
   desc "Create all multi-tenant databases"
   task :create => 'db:migrate' do
-    database_names.each do |db|
+    each_db do |db|
       begin
         puts("Creating #{db} database")
         quietly { Apartment::Database.create(db) }
@@ -17,7 +18,7 @@ apartment_namespace = namespace :apartment do
   desc "Migrate all multi-tenant databases"
   task :migrate => 'db:migrate' do
 
-    database_names.each do |db|
+    each_db do |db|
       begin
         puts("Migrating #{db} database")
         Apartment::Migrator.migrate db
@@ -30,7 +31,7 @@ apartment_namespace = namespace :apartment do
   desc "Seed all multi-tenant databases"
   task :seed => 'db:seed' do
 
-    database_names.each do |db|
+    each_db do |db|
       begin
         puts("Seeding #{db} database")
         Apartment::Database.process(db) do
@@ -46,7 +47,7 @@ apartment_namespace = namespace :apartment do
   task :rollback => 'db:rollback' do
     step = ENV['STEP'] ? ENV['STEP'].to_i : 1
 
-    database_names.each do |db|
+    each_db do |db|
       begin
         puts("Rolling back #{db} database")
         Apartment::Migrator.rollback db, step
@@ -63,7 +64,7 @@ apartment_namespace = namespace :apartment do
       version = ENV['VERSION'] ? ENV['VERSION'].to_i : nil
       raise 'VERSION is required' unless version
 
-      database_names.each do |db|
+      each_db do |db|
         begin
           puts("Migrating #{db} database up")
           Apartment::Migrator.run :up, db, version
@@ -78,7 +79,7 @@ apartment_namespace = namespace :apartment do
       version = ENV['VERSION'] ? ENV['VERSION'].to_i : nil
       raise 'VERSION is required' unless version
 
-      database_names.each do |db|
+      each_db do |db|
         begin
           puts("Migrating #{db} database down")
           Apartment::Migrator.run :down, db, version
@@ -99,6 +100,13 @@ apartment_namespace = namespace :apartment do
       end
     end
 
+  end
+
+  def each_db(&block)
+    Parallel.each(database_names, :in_threads=>Apartment.parallel_migration_threads) do |db|
+      ActiveRecord::Base.connection.reconnect!
+      block.call(db)
+    end
   end
 
   def database_names

cpoms/innkeeper

lib/tasks/apartment.rake
@@ -1,4 +1,5 @@
 require 'apartment/migrator'
+require 'parallel'
 
 apartment_namespace = namespace :apartment do
 
@@ -17,7 +18,7 @@ apartment_namespace = namespace :apartment do
   task :migrate do
     warn_if_tenants_empty
 
-    tenants.each do |tenant|
+    each_tenant do |tenant|
       begin
         Apartment::Migrator.migrate tenant
       rescue Apartment::TenantNotFound => e
@@ -30,7 +31,7 @@ apartment_namespace = namespace :apartment do
   task :seed do
     warn_if_tenants_empty
 
-    tenants.each do |tenant|
+    each_tenant do |tenant|
       begin
         Apartment::Tenant.switch(tenant) do
           Apartment::Tenant.seed
@@ -47,7 +48,7 @@ apartment_namespace = namespace :apartment do
 
     step = ENV['STEP'] ? ENV['STEP'].to_i : 1
 
-    tenants.each do |tenant|
+    each_tenant do |tenant|
       begin
         Apartment::Migrator.rollback tenant, step
       rescue Apartment::TenantNotFound => e
@@ -64,7 +65,7 @@ apartment_namespace = namespace :apartment do
       version = ENV['VERSION'] ? ENV['VERSION'].to_i : nil
       raise 'VERSION is required' unless version
 
-      tenants.each do |tenant|
+      each_tenant do |tenant|
         begin
           Apartment::Migrator.run :up, tenant, version
         rescue Apartment::TenantNotFound => e
@@ -80,7 +81,7 @@ apartment_namespace = namespace :apartment do
       version = ENV['VERSION'] ? ENV['VERSION'].to_i : nil
       raise 'VERSION is required' unless version
 
-      tenants.each do |tenant|
+      each_tenant do |tenant|
         begin
           Apartment::Migrator.run :down, tenant, version
         rescue Apartment::TenantNotFound => e
@@ -98,6 +99,12 @@ apartment_namespace = namespace :apartment do
         apartment_namespace['rollback'].invoke
         apartment_namespace['migrate'].invoke
       end
+    end
+  end
+
+  def each_tenant(&block)
+    Parallel.each(tenants, in_threads: Apartment.parallel_migration_threads) do |tenant|
+      block.call(tenant)
     end
   end
 